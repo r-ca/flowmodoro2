bd049ffe7e5dbe6c9c5f86e2de8943ba8dee97db
rca <nem@nem.x0.to>
2024-10-10 16:15:19 +0900

    debug

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { AppDataSource } from "./data-source";
 import authRoutes from './routes/auth';
 import taskRoutes from './routes/tasks';
+import { logger } from './middleware/logger';
 
 const app = express();
 
@@ -16,10 +17,16 @@ AppDataSource.initialize()
 app.use(cors());
 app.use(express.json());
 
+app.use(logger);
+
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-const PORT = process.env.PORT || 6000;
+app.get('/', (_req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
